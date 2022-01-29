9ec566984086f668c4ce89cf24b3d5884eaae69b
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-01-29 12:20:29 -0300

    build: generated the type definition files for graphql

---
+++ src/graphql.ts
@@ -0,0 +1,14 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export interface IQuery {
+    ping(): string | Promise<string>;
+}
+
+type Nullable<T> = T | null;
