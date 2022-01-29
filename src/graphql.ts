27a139be424c0be12ca00d268ee8d1d1b67ca261
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-01-29 18:44:36 -0300

    feat(graphql.gql & graphql.ts): updated the project types

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,8 +7,37 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export interface IQuery {
-    ping(): string | Promise<string>;
+export interface CreateUserInput {
+    email: string;
+    name: string;
+    password: string;
 }
 
+export interface BaseEntity {
+    createdAt: DateTime;
+    deletedAt?: Nullable<DateTime>;
+    id: string;
+    updatedAt: DateTime;
+}
+
+export interface IMutation {
+    createUser(input: CreateUserInput): UserEntity | Promise<UserEntity>;
+}
+
+export interface IQuery {
+    ping(): string | Promise<string>;
+    user(id: string): UserEntity | Promise<UserEntity>;
+}
+
+export interface UserEntity extends BaseEntity {
+    createdAt: DateTime;
+    deletedAt?: Nullable<DateTime>;
+    email: string;
+    id: string;
+    name: string;
+    roles: string[];
+    updatedAt: DateTime;
+}
+
+export type DateTime = any;
 type Nullable<T> = T | null;
