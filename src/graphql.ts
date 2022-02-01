12c4cee3deb0f54e2eee8d860b76f0feb7d9e8be
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-01 08:50:10 -0300

    feat(graphql.gql & graphql.ts): updated project types

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,6 +7,20 @@
 
 /* tslint:disable */
 /* eslint-disable */
+export enum ItemEntitySortFields {
+    name = "name"
+}
+
+export enum SortDirection {
+    ASC = "ASC",
+    DESC = "DESC"
+}
+
+export enum SortNulls {
+    NULLS_FIRST = "NULLS_FIRST",
+    NULLS_LAST = "NULLS_LAST"
+}
+
 export interface CreateItemInput {
     name: string;
 }
@@ -17,9 +31,45 @@ export interface CreateUserInput {
     password: string;
 }
 
+export interface CursorPaging {
+    after?: Nullable<ConnectionCursor>;
+    before?: Nullable<ConnectionCursor>;
+    first?: Nullable<number>;
+    last?: Nullable<number>;
+}
+
+export interface ItemEntityFilter {
+    and?: Nullable<ItemEntityFilter[]>;
+    name?: Nullable<StringFieldComparison>;
+    or?: Nullable<ItemEntityFilter[]>;
+}
+
+export interface ItemEntitySort {
+    direction: SortDirection;
+    field: ItemEntitySortFields;
+    nulls?: Nullable<SortNulls>;
+}
+
 export interface LoginInput {
     password: string;
     username: string;
+}
+
+export interface StringFieldComparison {
+    eq?: Nullable<string>;
+    gt?: Nullable<string>;
+    gte?: Nullable<string>;
+    iLike?: Nullable<string>;
+    in?: Nullable<string[]>;
+    is?: Nullable<boolean>;
+    isNot?: Nullable<boolean>;
+    like?: Nullable<string>;
+    lt?: Nullable<string>;
+    lte?: Nullable<string>;
+    neq?: Nullable<string>;
+    notILike?: Nullable<string>;
+    notIn?: Nullable<string[]>;
+    notLike?: Nullable<string>;
 }
 
 export interface BaseEntity {
@@ -37,14 +87,32 @@ export interface ItemEntity extends BaseEntity {
     updatedAt: DateTime;
 }
 
+export interface ItemEntityConnection {
+    edges: ItemEntityEdge[];
+    pageInfo: PageInfo;
+}
+
+export interface ItemEntityEdge {
+    cursor: ConnectionCursor;
+    node: ItemEntity;
+}
+
 export interface IMutation {
     createItem(input: CreateItemInput): ItemEntity | Promise<ItemEntity>;
     createUser(input: CreateUserInput): UserEntity | Promise<UserEntity>;
     login(input: LoginInput): TokenModel | Promise<TokenModel>;
 }
 
+export interface PageInfo {
+    endCursor?: Nullable<ConnectionCursor>;
+    hasNextPage?: Nullable<boolean>;
+    hasPreviousPage?: Nullable<boolean>;
+    startCursor?: Nullable<ConnectionCursor>;
+}
+
 export interface IQuery {
     item(id: string): ItemEntity | Promise<ItemEntity>;
+    items(filter?: Nullable<ItemEntityFilter>, paging?: Nullable<CursorPaging>, sorting?: Nullable<ItemEntitySort[]>): ItemEntityConnection | Promise<ItemEntityConnection>;
     me(): UserEntity | Promise<UserEntity>;
     ping(): string | Promise<string>;
     user(id: string): UserEntity | Promise<UserEntity>;
@@ -65,5 +133,6 @@ export interface UserEntity extends BaseEntity {
     updatedAt: DateTime;
 }
 
+export type ConnectionCursor = any;
 export type DateTime = any;
 type Nullable<T> = T | null;
