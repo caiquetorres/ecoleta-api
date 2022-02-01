39fcdefa4102f8463bcd96b7635f5202772f4e5b
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-01 08:20:36 -0300

    feat(item.resolver.ts & item.service.ts): created the "createOne" and the "getOne" methods

--- src/item/item.resolver.ts
+++ src/item/item.resolver.ts
@@ -1,6 +1,12 @@
-import { Resolver } from '@nestjs/graphql'
+import { ParseUUIDPipe } from '@nestjs/common'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+
+import { Protect } from '../common/decorators/protect/protect.decorator'
 
 import { ItemEntity } from './entities/item.entity'
+
+import { RoleEnum } from '../common/models/role.enum'
+import { CreateItemInput } from './dtos/create-item.input'
 
 import { ItemService } from './item.service'
 
@@ -13,4 +19,40 @@ import { ItemService } from './item.service'
 @Resolver(() => ItemEntity)
 export class ItemResolver {
   constructor(private readonly itemService: ItemService) {}
+
+  /**
+   * Mutation responsible for creating a new entity.
+   *
+   * @param input defines an object that contains all the entity data.
+   * @returns an object that represents the created entity.
+   */
+  @Protect(RoleEnum.admin)
+  @Mutation(() => ItemEntity, {
+    name: 'createItem',
+    description: 'Mutation responsible for creating a new entity.',
+  })
+  createOne(
+    @Args('input', { type: () => CreateItemInput })
+    input: CreateItemInput,
+  ) {
+    return this.itemService.createOne(input)
+  }
+
+  /**
+   * Query responsible for finding one entity based on the id parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the found entity.
+   */
+  @Query(() => ItemEntity, {
+    name: 'item',
+    description:
+      'Query responsible for finding one entity based on the id parameter.',
+  })
+  getOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+  ) {
+    return this.itemService.getOne(id)
+  }
 }
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common'
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
 import { ItemEntity } from './entities/item.entity'
+
+import { CreateItemInput } from './dtos/create-item.input'
 
 /**
  * Service that deals with all the business logic related with the
@@ -14,4 +20,51 @@ export class ItemService {
     @InjectRepository(ItemEntity)
     private readonly repository: Repository<ItemEntity>,
   ) {}
+
+  /**
+   * Method responsible for creating a new entity.
+   *
+   * @param input defines an object that contains all the entity data.
+   * @returns an object that represents the created entity.
+   */
+  async createOne(input: CreateItemInput) {
+    if (await this.repository.findOne({ name: input.name })) {
+      throw new ConflictException(
+        'An item with that name has already been registered',
+      )
+    }
+
+    const item = new ItemEntity(input)
+    return this.repository.save(item)
+  }
+
+  /**
+   * Method responsible for finding one entity based on the `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the found entity.
+   */
+  async getOne(id: string) {
+    const item = await this.findOneById(id)
+
+    if (!item) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${ItemEntity.name} was not found`,
+      )
+    }
+
+    return item
+  }
+
+  /**
+   * Method responsible for finding one entity based on the `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the found entity.
+   */
+  findOneById(id: string) {
+    return this.repository.findOne({ id })
+  }
 }
