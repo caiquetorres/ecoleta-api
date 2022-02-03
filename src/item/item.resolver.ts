5a9c9f5969a527f35a368afe60ee77d1511a7d12
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-03 07:28:03 -0300

    feat(item/*): created the updateOne, deleteOne, disableOne and enableOne methods

--- src/item/item.resolver.ts
+++ src/item/item.resolver.ts
@@ -8,6 +8,7 @@ import { ItemEntity } from './entities/item.entity'
 import { RoleEnum } from '../common/models/role.enum'
 import { CreateItemInput } from './dtos/create-item.input'
 import { QueryItemsArgs } from './dtos/query-items.args'
+import { UpdateItemInput } from './dtos/update-item.input'
 
 import { ItemService } from './item.service'
 
@@ -19,7 +20,7 @@ import { ItemService } from './item.service'
  */
 @Resolver(() => ItemEntity)
 export class ItemResolver {
-  constructor(private readonly itemService: ItemService) {}
+  constructor(private readonly service: ItemService) {}
 
   /**
    * Mutation responsible for creating a new entity.
@@ -36,7 +37,7 @@ export class ItemResolver {
     @Args('input', { type: () => CreateItemInput })
     input: CreateItemInput,
   ) {
-    return this.itemService.createOne(input)
+    return this.service.createOne(input)
   }
 
   /**
@@ -54,11 +55,11 @@ export class ItemResolver {
     @Args('id', { nullable: false }, ParseUUIDPipe)
     id: string,
   ) {
-    return this.itemService.getOne(id)
+    return this.service.getOne(id)
   }
 
   /**
-   * Method responsible for finding several entities based on the
+   * Query responsible for finding several entities based on the
    * `query` parameter.
    *
    * @param query defines an object that contains the data needed for
@@ -67,11 +68,94 @@ export class ItemResolver {
    */
   @Query(() => QueryItemsArgs.ConnectionType, {
     name: 'items',
+    description:
+      'Query responsible for finding several entities based on the input data.',
   })
   getMany(
     @Args()
     query: QueryItemsArgs,
   ) {
-    return this.itemService.getMany(query)
+    return this.service.getMany(query)
+  }
+
+  /**
+   * Mutation responsible for updating some entity based on the sent
+   * `input` parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @param input defines an object that represents the entity new
+   * data.
+   * @returns an object that represents the updated entity.
+   */
+  @Protect(RoleEnum.admin)
+  @Mutation(() => ItemEntity, {
+    name: 'updateItem',
+    description:
+      'responsible for updating some entity based on the sent `input` parameter.',
+  })
+  updateOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+    @Args('input', { type: () => UpdateItemInput })
+    input: UpdateItemInput,
+  ) {
+    return this.service.updateOne(id, input)
+  }
+
+  /**
+   * Mutation responsible for deleting some entity based on the sent `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the disabled entity.
+   */
+  @Mutation(() => ItemEntity, {
+    name: 'deleteItem',
+    description:
+      'Mutation responsible for deleting some entity based on the `id` parameter.',
+  })
+  deleteOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+  ): Promise<ItemEntity> {
+    return this.service.deleteOne(id)
+  }
+
+  /**
+   * Mutation responsible for disabling some entity based on the sent `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the disabled entity.
+   */
+  @Mutation(() => ItemEntity, {
+    name: 'disableItem',
+    description:
+      'Mutation responsible for disabling some entity based on the `id` parameter.',
+  })
+  disableOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+  ) {
+    return this.service.disableOne(id)
+  }
+
+  /**
+   * Mutation responsible for enabling some entity based on the sent `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the enabled entity.
+   */
+  @Mutation(() => ItemEntity, {
+    name: 'enableItem',
+    description:
+      'Mutation responsible for enabling some entity based on the `id` parameter.',
+  })
+  enableOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+  ) {
+    return this.service.enableOne(id)
   }
 }
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,3 @@
-import { TypeOrmQueryService } from '@nestjs-query/query-typeorm'
 import {
   ConflictException,
   Injectable,
@@ -11,6 +10,9 @@ import { ItemEntity } from './entities/item.entity'
 
 import { CreateItemInput } from './dtos/create-item.input'
 import { QueryItemsArgs } from './dtos/query-items.args'
+import { UpdateItemInput } from './dtos/update-item.input'
+
+import { TypeOrmQueryService } from '../common/services/typeorm-query.service'
 
 /**
  * Service that deals with all the business logic related with the
@@ -20,7 +22,7 @@ import { QueryItemsArgs } from './dtos/query-items.args'
 export class ItemService extends TypeOrmQueryService<ItemEntity> {
   constructor(
     @InjectRepository(ItemEntity)
-    private readonly repository: Repository<ItemEntity>,
+    repository: Repository<ItemEntity>,
   ) {
     super(repository)
   }
@@ -50,7 +52,7 @@ export class ItemService extends TypeOrmQueryService<ItemEntity> {
    * @returns an object that represents the found entity.
    */
   async getOne(id: string) {
-    const item = await this.findById(id)
+    const item = await this.findOneById(id)
 
     if (!item) {
       throw new NotFoundException(
@@ -69,10 +71,92 @@ export class ItemService extends TypeOrmQueryService<ItemEntity> {
    * filtering, sorting and paginating the found entity.
    * @returns an object that contains all the found data.
    */
-  async getMany(query: QueryItemsArgs) {
+  getMany(query: QueryItemsArgs) {
     return QueryItemsArgs.ConnectionType.createFromPromise(
       (query) => this.query(query),
       query,
     )
+  }
+
+  /**
+   * Method responsible for updating some entity based on the sent
+   * `input` parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @param input defines an object that represents the entity new
+   * data.
+   * @returns an object that represents the updated entity.
+   */
+  async updateOne(id: string, input: UpdateItemInput) {
+    const item = await this.repository.findOne(id)
+
+    if (!item) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${ItemEntity.name} was not found`,
+      )
+    }
+
+    return this.repository.save({
+      ...item,
+      ...input,
+    })
+  }
+
+  /**
+   * Method that deletes the entity based on the sent `id` paramter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the deleted entity.
+   */
+  async deleteOne(id: string) {
+    const item = await this.repository.findOne(id)
+
+    if (!item) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${ItemEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.delete(item)
+    return this.repository.findOne(id)
+  }
+
+  /**
+   * Method that disables the entity based on the sent `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the disabled entity.
+   */
+  async disableOne(id: string) {
+    const item = await this.repository.findOne(id)
+
+    if (!item) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${ItemEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.softDelete(item)
+    return this.repository.findOne(id)
+  }
+
+  /**
+   * Method that enables the entity based on the sent `ìd` parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the enabled entity.
+   */
+  async enableOne(id: string) {
+    const item = await this.repository.findOne(id)
+
+    if (!item) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${ItemEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.restore(item)
+    return this.repository.findOne(id)
   }
 }
