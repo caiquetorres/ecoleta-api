4e1f5058e318fa8dfbfd5b3dae9c71567e257be2
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-03 07:42:59 -0300

    feat(user/*): created the updateOne, deleteOne, disableOne and enableOne methods

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -8,6 +8,7 @@ import { JwtGuard } from '../common/guards/jwt/jwt.guard'
 import { UserEntity } from './entities/user.entity'
 
 import { CreateUserInput } from './dtos/create-user.input'
+import { UpdateUserInput } from './dtos/update-user.input'
 
 import { UserService } from './user.service'
 
@@ -19,7 +20,7 @@ import { UserService } from './user.service'
  */
 @Resolver(() => UserEntity)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly service: UserService) {}
 
   /**
    * Mutation responsible for creating a new entity.
@@ -35,7 +36,7 @@ export class UserResolver {
     @Args('input', { type: () => CreateUserInput })
     input: CreateUserInput,
   ) {
-    return this.userService.createOne(input)
+    return this.service.createOne(input)
   }
 
   /**
@@ -43,6 +44,8 @@ export class UserResolver {
    * parameter.
    *
    * @param id defines the entity unique identifier.
+   * @param currentUser defines an object that represenst the user that is
+   * requesting this action.
    * @returns an object that represents the found entity.
    */
   @UseGuards(JwtGuard)
@@ -57,7 +60,7 @@ export class UserResolver {
     @CurrentUser()
     currentUser: UserEntity,
   ) {
-    return this.userService.getOne(id, currentUser)
+    return this.service.getOne(id, currentUser)
   }
 
   /**
@@ -75,6 +78,80 @@ export class UserResolver {
     @CurrentUser()
     currentUser: UserEntity,
   ) {
-    return this.userService.getOne(currentUser.id, currentUser)
+    return this.service.getOne(currentUser.id, currentUser)
+  }
+
+  /**
+   * Mutation responsible for updating some entity data.
+   *
+   * @param id defines the entity unique identifier.
+   * @param input defines an object that contains all the new entity data.
+   * @param currentUser defines an object that represenst the user that is
+   * requesting this action.
+   * @returns an object that represents the created entity.
+   */
+  updateOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+    @Args('input', {
+      nullable: false,
+      type: () => UpdateUserInput,
+    })
+    input: UpdateUserInput,
+    @CurrentUser()
+    currentUser: UserEntity,
+  ) {
+    return this.service.updateOne(id, input, currentUser)
+  }
+
+  /**
+   * Mutation responsible for deleting some entity.
+   *
+   * @param id defines the entity unique identifier.
+   * @param currentUser defines an object that represenst the user that is
+   * requesting this action.
+   * @returns an object that represents the created entity.
+   */
+  deleteOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+    @CurrentUser()
+    currentUser: UserEntity,
+  ) {
+    return this.service.deleteOne(id, currentUser)
+  }
+
+  /**
+   * Mutation responsible for disabling some entity.
+   *
+   * @param id defines the entity unique identifier.
+   * @param currentUser defines an object that represenst the user that is
+   * requesting this action.
+   * @returns an object that represents the created entity.
+   */
+  disableOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+    @CurrentUser()
+    currentUser: UserEntity,
+  ) {
+    return this.disableOne(id, currentUser)
+  }
+
+  /**
+   * Mutation responsible for enabling some entity.
+   *
+   * @param id defines the entity unique identifier.
+   * @param currentUser defines an object that represenst the user that is
+   * requesting this action.
+   * @returns an object that represents the created entity.
+   */
+  enableOne(
+    @Args('id', { nullable: false }, ParseUUIDPipe)
+    id: string,
+    @CurrentUser()
+    currentUser: UserEntity,
+  ) {
+    return this.enableOne(id, currentUser)
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,3 @@
-import { TypeOrmQueryService } from '@nestjs-query/query-typeorm'
 import {
   ConflictException,
   ForbiddenException,
@@ -12,7 +11,9 @@ import { UserEntity } from './entities/user.entity'
 
 import { RoleEnum } from '../common/models/role.enum'
 import { CreateUserInput } from './dtos/create-user.input'
+import { UpdateUserInput } from './dtos/update-user.input'
 
+import { TypeOrmQueryService } from '../common/services/typeorm-query.service'
 import { PasswordService } from '../password/password.service'
 import { PermissionService } from '../permission/permission.service'
 
@@ -24,7 +25,7 @@ import { PermissionService } from '../permission/permission.service'
 export class UserService extends TypeOrmQueryService<UserEntity> {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly repository: Repository<UserEntity>,
+    repository: Repository<UserEntity>,
     private readonly passwordService: PasswordService,
     private readonly permissionService: PermissionService,
   ) {
@@ -68,13 +69,119 @@ export class UserService extends TypeOrmQueryService<UserEntity> {
       )
     }
 
-    const user = await this.findById(id)
+    const user = await this.findOneById(id)
     if (!user) {
       throw new NotFoundException(
         `The entity identified by \'${id}\' of type \'${UserEntity.name}\' was not found`,
       )
     }
     return user
+  }
+
+  /**
+   * Method responsible for updating some entity based on the sent
+   * `input` parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @param input defines an object that represents the entity new
+   * data.
+   * @returns an object that represents the updated entity.
+   */
+  async updateOne(id: string, input: UpdateUserInput, currentUser: UserEntity) {
+    if (!this.permissionService.hasPermission(id, currentUser)) {
+      throw new ForbiddenException(
+        'You have no permission to access these sources.',
+      )
+    }
+
+    const user = await this.repository.findOne(id)
+
+    if (!user) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${UserEntity.name} was not found`,
+      )
+    }
+
+    return this.repository.save({
+      ...user,
+      ...input,
+    })
+  }
+
+  /**
+   * Method that deletes the entity based on the sent `id` paramter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the deleted entity.
+   */
+  async deleteOne(id: string, currentUser: UserEntity) {
+    if (!this.permissionService.hasPermission(id, currentUser)) {
+      throw new ForbiddenException(
+        'You have no permission to access these sources.',
+      )
+    }
+
+    const user = await this.repository.findOne(id)
+
+    if (!user) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${UserEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.delete(user)
+    return this.repository.findOne(id)
+  }
+
+  /**
+   * Method that disables the entity based on the sent `id`
+   * parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the disabled entity.
+   */
+  async disableOne(id: string, currentUser: UserEntity) {
+    if (!this.permissionService.hasPermission(id, currentUser)) {
+      throw new ForbiddenException(
+        'You have no permission to access these sources.',
+      )
+    }
+
+    const user = await this.repository.findOne(id)
+
+    if (!user) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${UserEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.softDelete(user)
+    return this.repository.findOne(id)
+  }
+
+  /**
+   * Method that enables the entity based on the sent `ìd` parameter.
+   *
+   * @param id defines the entity unique identifier.
+   * @returns an object that represents the enabled entity.
+   */
+  async enableOne(id: string, currentUser: UserEntity) {
+    if (!this.permissionService.hasPermission(id, currentUser)) {
+      throw new ForbiddenException(
+        'You have no permission to access these sources.',
+      )
+    }
+
+    const user = await this.repository.findOne(id)
+
+    if (!user) {
+      throw new NotFoundException(
+        `The entity identified by ${id} of type ${UserEntity.name} was not found`,
+      )
+    }
+
+    await this.repository.restore(user)
+    return this.repository.findOne(id)
   }
 
   /**
