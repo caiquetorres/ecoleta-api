c844d5425e7155acc6aaea64dfeca0173e6902fb
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-05 21:37:21 -0300

    feat(item.entity.ts): created the "image" and the "imageId" properties

--- src/item/entities/item.entity.ts
+++ src/item/entities/item.entity.ts
@@ -1,38 +1,57 @@
 import { FilterableField } from '@nestjs-query/query-graphql'
 import { ObjectType } from '@nestjs/graphql'
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, JoinColumn, OneToOne, RelationId } from 'typeorm'
 
 import { BaseEntity } from '../../common/entities/base.entity'
+import { ImageEntity } from '../../image/entities/image.entity'
+
+import { IItem } from '../item.interface'
 
 /**
  * Entity that represents the `item` entity.
  */
+@Entity('item')
 @ObjectType({
   implements: () => [BaseEntity],
 })
-@Entity('item')
-export class ItemEntity extends BaseEntity {
+export class ItemEntity extends BaseEntity implements IItem {
   //#region Public properties
 
   /**
-   * Property that defines a `string` object that represents the item
-   * name.
+   * @inheritdoc
    */
-  @FilterableField({
-    nullable: false,
-    description:
-      'Property that defines a `string` object that represents the item name.',
-  })
+  @FilterableField({ nullable: false })
   @Column({
     type: 'varchar',
-    length: '32',
+    length: 32,
     nullable: false,
   })
   name: string
 
+  /**
+   * Property that defines a `string` that represents the related
+   * `image` entity id.
+   */
+  @FilterableField({ nullable: true })
+  @RelationId((item: ItemEntity) => item.image)
+  imageId: string
+
+  //#region Relations
+
+  /**
+   * @inheritdoc
+   */
+  @JoinColumn()
+  @OneToOne(() => ImageEntity, {
+    cascade: true,
+  })
+  image: ImageEntity
+
   //#endregion
 
-  constructor(partial: Partial<ItemEntity>) {
+  //#endregion
+
+  constructor(partial: Partial<IItem>) {
     super()
     Object.assign(this, partial)
   }
