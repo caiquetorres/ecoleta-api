20452daf78c3392a76285c8427bb42ca5686aaf7
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-05 21:34:51 -0300

    feat(image.entity.ts & create-image.input.ts & update-image.input.ts): created the "url" and the "title" properties

--- src/image/dtos/update-image.input.ts
+++ src/image/dtos/update-image.input.ts
@@ -1,13 +1,32 @@
-import { InputType } from '@nestjs/graphql'
+import { Field, InputType } from '@nestjs/graphql'
 
-import { CreateImageInput } from './create-image.input'
+import { IImage } from '../image.interface'
+import { IsOptional, IsString, IsUrl } from 'class-validator'
 
 /**
  * Input that handles all the data needed to update some `image`
  * entity.
  */
-@InputType({
-  description:
-    'Input that handles all the data needed to update some `image` entity.',
-})
-export class UpdateImageInput extends CreateImageInput {}
+@InputType()
+export class UpdateImageInput implements IImage {
+  //#region Public properties
+
+  /**
+   * @inheritdoc
+   */
+  @Field({ nullable: false })
+  @IsOptional()
+  @IsUrl({}, { message: 'It is required to send a valid url' })
+  @IsString({ message: 'It is required to send a valid string' })
+  url: string
+
+  /**
+   * @inheritdoc
+   */
+  @Field({ nullable: true })
+  @IsOptional()
+  @IsString({ message: 'It is required to send a valid string' })
+  title: string
+
+  //#endregion
+}
