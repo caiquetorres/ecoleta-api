d320db47c396ad1d97318a444c68b2f3e88b3703
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-09 17:36:52 -0300

    feat(point.module.ts): added the PointEntity and the AddressEntity classes in the PointModule imports

--- src/point/point.module.ts
+++ src/point/point.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AddressEntity } from './entities/address.entity'
+import { PointEntity } from './entities/point.entity'
 
 import { PointService } from './point.service'
 
 import { PointResolver } from './point.resolver'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PointEntity, AddressEntity])],
   providers: [PointService, PointResolver],
 })
 export class PointModule {}
