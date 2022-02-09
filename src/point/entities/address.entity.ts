7be31cf9d3bebfc9d49d273f998e79c9bcf339bd
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-02-09 17:32:27 -0300

    feat(address.entity.ts): created this class to represent the applications address entity

---
+++ src/point/entities/address.entity.ts
@@ -0,0 +1,80 @@
+import { FilterableField } from '@nestjs-query/query-graphql'
+import { ObjectType } from '@nestjs/graphql'
+import { Column, Entity } from 'typeorm'
+
+import { BaseEntity } from '../../common/entities/base.entity'
+
+import { IAddress } from '../address.interface'
+
+/**
+ * Entity that represents the `address` entity.
+ */
+@Entity('address')
+@ObjectType({
+  implements: () => [BaseEntity],
+})
+export class AddressEntity extends BaseEntity implements IAddress {
+  //#region Public properties
+
+  /**
+   * @inheritdoc
+   */
+  @FilterableField({ nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  street: string
+
+  /**
+   * @inheritdoc
+   */
+  @FilterableField({ nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  district: string
+
+  /**
+   * @inheritdoc
+   */
+  @FilterableField({ nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  city: string
+
+  /**
+   * @inheritdoc
+   */
+  @FilterableField({ nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  state: string
+
+  /**
+   * @inheritdoc
+   */
+  @FilterableField({ nullable: false })
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false,
+  })
+  country: string
+
+  //#endregion
+
+  constructor(partial: Partial<IAddress>) {
+    super()
+    Object.assign(this, partial)
+  }
+}
